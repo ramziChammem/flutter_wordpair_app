f2685b9545b65a0f3d7bc5c3585ac2ff68251f84
ramziChammem <ramzichammem69@gmail.com>
2021-05-07 23:09:29 +0100

    working an the word pars list

--- wordpair_generator/lib/main.dart
+++ wordpair_generator/lib/main.dart
@@ -1,23 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.deepPurple[700]),
-      home: Scaffold(
-        appBar:  AppBar(
-          centerTitle: true,
-          title: Text("WordPair Generator", textAlign: TextAlign.center,),
-          
+        theme: ThemeData(primaryColor: Colors.deepPurple[700]),
+        home: RandomWord());
+  }
+}
+
+class RandomWord extends StatefulWidget {
+  @override
+  _RandomWordState createState() => _RandomWordState();
+}
+
+class _RandomWordState extends State<RandomWord> {
+  final _randomWordPairs = <WordPair>[];
+
+  Widget _buildList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (context, item) {
+        if (item.isOdd) {
+          return Divider();
+        }
+        final index = item ~/ 2;
+        if (index >= _randomWordPairs.length) {
+          _randomWordPairs.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_randomWordPairs[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(title: Text(pair.asPascalCase, style: TextStyle(
+      fontSize: 20
+    )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "WordPair Generator",
+          textAlign: TextAlign.center,
         ),
-        body: Center(
-          child: Text("Heleleews again and again")
-        )
-      )
+      ),
+      body: _buildList(),
     );
   }
 }
