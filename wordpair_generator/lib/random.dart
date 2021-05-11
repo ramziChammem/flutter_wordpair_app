53642b474fc36d3e7b8d8d18b50fc792f53600f4
ramziChammem <ramzichammem69@gmail.com>
2021-05-11 22:17:11 +0100

    wordpair application is done

--- wordpair_generator/lib/random.dart
+++ wordpair_generator/lib/random.dart
@@ -27,13 +27,45 @@ class _RandomWordState extends State<RandomWord> {
 
   Widget _buildRow(WordPair pair) {
     final addedWordPair = _myFavorite.contains(pair);
+    void addToFavorite() {
+      setState(() {
+        if (addedWordPair) {
+          _myFavorite.remove(pair);
+        } else {
+          _myFavorite.add(pair);
+        }
+      });
+    }
+
     return ListTile(
-        title: Text(pair.asPascalCase, style: TextStyle(fontSize: 20)),
-        trailing: Icon(Icons.star),
-        );
+      title: Text(pair.asPascalCase, style: TextStyle(fontSize: 20)),
+      trailing: Icon(addedWordPair ? Icons.star : Icons.star_border,
+          color: addedWordPair ? Colors.yellow : Colors.yellow),
+      onTap: addToFavorite,
+    );
   }
 
   @override
+  void _addSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles = _myFavorite.map((WordPair elem) {
+        return ListTile(
+            title: Text(elem.asPascalCase, style: TextStyle(fontSize: 20)));
+      });
+      final List<Widget> divided = ListTile.divideTiles(tiles: tiles).toList();
+
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Saved WordPairs"),
+        ),
+        body: ListView(
+          children: divided, 
+        )
+      );
+    }));
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -42,6 +74,12 @@ class _RandomWordState extends State<RandomWord> {
           "WordPair Generator",
           textAlign: TextAlign.center,
         ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: _addSaved,
+          ),
+        ],
       ),
       body: _buildList(),
     );
